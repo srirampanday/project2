142b6f688983a8937fe25e78d1a126560825591f
srirampanday <srirampanday@gmail.com>
2023-06-24 02:34:52 +0800

    savav

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, color } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Canvas = ({ canvasRef, ctx, color }) => {
   const [isDrawing, setIsDrawing] = useState(false);
